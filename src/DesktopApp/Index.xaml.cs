4bb7e4ec92dd0fa96c0d3b37fa3280e45010bdb8
zh6335901 <zh6335901@hotmail.com>
2022-08-03 23:53:15 +0800

    feat: Implements login function by WebView2

---
+++ src/DesktopApp/Index.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace DesktopApp
+{
+    public partial class Index : Window
+    {
+        public Index()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var login = new Login();
+            login.CodeReceived += Login_CodeReceived;
+
+            login.ShowDialog();
+        }
+
+        private void Login_CodeReceived(object? sender, CodeReceivedEventArgs e)
+        {
+            var userProfile = new UserProfile(e.Code);
+            userProfile.Show();
+
+            Close();
+        }
+    }
+}
---
+++ src/DesktopApp/UserProfile.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+namespace DesktopApp
+{
+    public partial class UserProfile : Window
+    {
+        private readonly string _authCode;
+        private readonly CasdoorApi _casdoorApi;
+
+        public UserProfile(string authCode)
+        {
+            InitializeComponent();
+
+            _authCode = authCode;
+            _casdoorApi = new CasdoorApi(CasdoorVariables.Domain);
+
+            Loaded += UserProfile_Loaded;
+        }
+
+        private async void UserProfile_Loaded(object sender, RoutedEventArgs e)
+        {
+            var token = await _casdoorApi.RequestToken(
+                CasdoorVariables.ClientId, 
+                CasdoorVariables.ClientSecret, 
+                _authCode
+            );
+
+            // Assume request token and get user process is in happy path..
+            var user = await _casdoorApi.GetUserInfo(token!);
+            Username.Text = user!.Name;
+        }
+
+        private void LogoutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var index = new Index();
+            index.Show();
+
+            Close();
+        }
+    }
+}
