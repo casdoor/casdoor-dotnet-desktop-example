25bccd4b74e336ad4188d48e10de385ea36f70b0
DacongDA <dacongda1@sina.com>
2024-02-19 00:15:48 +0800

    feat: use dotnet sdk to replace self define api

--- src/DesktopApp/CasdoorVariables.cs
+++ src/DesktopApp/CasdoorVariables.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.Cryptography;
+﻿using Casdoor.Client;
+using System.Net.Http;
+using System.Security.Cryptography;
 
 namespace DesktopApp
 {
@@ -8,5 +10,22 @@ namespace DesktopApp
         public const string AppName = "app-casnode";
         public const string ClientId = "014ae4bd048734ca2dea";
         public const string CallbackUrl = "casdoor://callback";
+        public static CasdoorOptions options = new CasdoorOptions
+        {
+            Endpoint = Domain,
+            OrganizationName = "casbin",
+            ApplicationName = AppName,
+            ApplicationType = "webapp", // webapp, webapi or native
+            ClientId = ClientId,
+
+            // Optional: The callback path that the client will be redirected to
+            // after the user has authenticated. default is "/casdoor/signin-callback"
+            CallbackPath = CallbackUrl,
+            // Optional: Whether require https for casdoor endpoint
+            RequireHttpsMetadata = false,
+            // Optional: The scopes that the client is requesting.
+            Scope = "profile"
+        };
+        public static CasdoorClient casdoorClient = new CasdoorClient(new HttpClient(), options);
     }
 }
